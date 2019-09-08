6febff9fcc5a7d0c7bfb31c038c6ed9877934c35
Technetium <Technetium1@users.noreply.github.com>
2019-09-08 14:17:41 -0400

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -3,6 +3,9 @@ TITLE ChocolateyUpdate Compiler
 python -V > NUL 2> NUL
 if errorlevel 1 echo PYTHON NOT IN PATH! && PAUSE && EXIT
 cd %~dp0
+python -m pip install -U pyinstaller
+python -m pip install -r %~dp0requirements.txt
 pyinstaller -F -i choco.ico --clean ChocolateyUpdate.py
+ECHO Done! File is located in %~dp0dist
 pause
 exit

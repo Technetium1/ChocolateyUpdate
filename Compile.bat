12c9e17508059bf81bcd02e285eeacef12520c3c
Technetium <Technetium1@users.noreply.github.com>
2019-09-08 13:28:04 -0400

    Add title & check for python in path

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,8 @@
 @echo off
+TITLE ChocolateyUpdate Compiler
+python -V > NUL 2> NUL
+if errorlevel 1 echo PYTHON NOT IN PATH! && PAUSE && EXIT
+cd %~dp0
 pyinstaller -F -i choco.ico --clean ChocolateyUpdate.py
 pause
 exit

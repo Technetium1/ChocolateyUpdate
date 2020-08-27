1868e8d9f7aee9196be6e0e28eee6c89df932bdd
Technetium <Technetium1@users.noreply.github.com>
2020-08-27 15:20:37 -0400

    Enable automatic updates
    Only check for updates when running from the executable

--- ChocolateyUpdate.py
+++ ChocolateyUpdate.py
@@ -1,19 +1,22 @@
 # https://github.com/Technetium1
 # Licensed under The Unlicense [unlicense.org]
 from os import system
+from os import remove
+from os import rename
 from shutil import which
 from pathlib import Path
 from ctypes import windll
 import urllib3
+import json
 import certifi
 import sys
 
-version = '1.4'
+version = "2.0"
 
 
 def printascii():
-    system('cls')
-    system('title Tech\'s Chocolatey Updater V' + version)
+    system("cls")
+    system("title Tech\'s Chocolatey Updater V" + version)
     print(r"""
 .---..-- .- .  .\\ ,-.    .- .  . .-.  .- .-. .   .. .---..--.   .  .  . .-. .-.  .. .---..--.-.
   |  |  /   |  |  (   `  /   |  |/   \/  /   \|  /  \  |  |   \ /   |  | |  )|  \/  \  |  |  |  )
@@ -21,7 +24,7 @@ def printascii():
   |  |  \   |  |  .   )  \   |  |\   /\  \   /|  |  |  |  |    |    |  | |   |  /|  |  |  |  |  \
   '  '-- `- '  '   `-'    `- '  ' '-'  `- `-' `--'  '  '  `--  '    `--` '   '-' '  '  '  '--'   '
           """)
-    print('\n' + 'V' + version + '\n')
+    print("\n" + "v" + version + "\n")
 
 
 def nopackagefile():
@@ -42,35 +45,36 @@ Since it didn't exist the default was downloaded!
 Edit out any unwanted programs and then rerun!
 ####################################################
         """)
-    packagesurl = 'https://raw.githubusercontent.com/Technetium1/ChocolateyUpdate/master/ChocolateyPackages.txt'
+    packagesurl = "https://raw.githubusercontent.com/Technetium1/ChocolateyUpdate/master/ChocolateyPackages.txt"
     http = urllib3.PoolManager(ca_certs=certifi.where())
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     r = http.request(
-        'GET',
+        "GET",
         packagesurl,
         timeout=urllib3.Timeout(connect=15.0, read=15.0),
-        retries=4)
-    with open('ChocolateyPackages.txt', 'wb') as chocopkgs:
+        retries=4,
+        redirect=False)
+    with open("ChocolateyPackages.txt", "wb") as chocopkgs:
         chocopkgs.write(r.data)
-    system('pause')
+    system("pause")
     raise SystemExit
 
 
 def installpackages():
-    with open('ChocolateyPackages.txt', 'r') as file:
-        updates = file.read().replace('\n', ' ')
+    with open("ChocolateyPackages.txt", "r") as file:
+        updates = file.read().replace("\n", " ")
     print("FOUND PACKAGES: " + updates)
-    up = 'choco upgrade -y '
-    print('\n\n\n~~~~~~~~~~~~~~~~~~~~\nUPDATING CHOCOLATEY!\n~~~~~~~~~~~~~~~~~~~~\n\n\n')
-    system(up + 'chocolatey')
-    print('\n\n\n~~~~~~~~~~~~~~~~~~\nUPDATING PACKAGES!\n~~~~~~~~~~~~~~~~~~\n\n\n')
+    up = "choco upgrade -y "
+    print("\n\n\n~~~~~~~~~~~~~~~~~~~~\nUPDATING CHOCOLATEY!\n~~~~~~~~~~~~~~~~~~~~\n\n\n")
+    system(up + "chocolatey")
+    print("\n\n\n~~~~~~~~~~~~~~~~~~\nUPDATING PACKAGES!\n~~~~~~~~~~~~~~~~~~\n\n\n")
     system(up + updates)
-    print('\nSUCCESS!\n')
-    system('pause')
+    print("\nSUCCESS!\n")
+    system("pause")
 
 
 def checkforpackages():
-    if Path('ChocolateyPackages.txt').exists():
+    if Path("ChocolateyPackages.txt").exists():
         installpackages()
     else:
         nopackagefile()
@@ -81,20 +85,71 @@ def admincheck():
         printascii()
         stopcontrolledfolderaccess = "powershell.exe -Command Set-MpPreference -EnableControlledFolderAccess Disabled > nul 2>&1"
         system(stopcontrolledfolderaccess)
+        if getattr(sys, "frozen", True):
+            selfupdate()
         if which("choco") is not None:
             checkforpackages()
         else:
             print("NO CHOCOLATEY INSTALLED!")
             installchoco = "start /wait powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
             system(installchoco)
-            print('\nPlease restart the program!\n')
-            system('pause')
+            print("\nPlease restart the program!\n")
+            system("pause")
             raise SystemExit
     else:
         printascii()
         print("ATTEMPTING TO GET ADMINISTRATOR PERMISSIONS!")
         windll.shell32.ShellExecuteW(
             None, "runas", sys.executable, sys.argv[0], None, 1)
+
+
+def selfupdate():
+    updateurl = "https://api.github.com/repos/Technetium1/ChocolateyUpdate/releases/latest"
+    http = urllib3.PoolManager(ca_certs=certifi.where())
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    r = http.request(
+        "GET",
+        updateurl,
+        headers={"User-Agent": "curl", "Accept": "application/vnd.github.v3+json"},
+        timeout=urllib3.Timeout(connect=6.0, read=6.0),
+        retries=4,
+        redirect=False)
+    updateresult = json.loads(r.data.decode("utf-8"))
+    currentversion = updateresult["tag_name"]
+    downloadlink = updateresult["assets"][0]["browser_download_url"]
+    if currentversion > version:
+        print("Newer version found on GitHub!")
+    elif currentversion < version:
+        print("Version newer than what was found on GitHub!")
+    elif currentversion == version:
+        print("Already at latest GitHub release!")
+    else:
+        print("Something went wrong checking for updates! If this continues report to https://github.com/Technetium1/ChocolateyUpdate")
+        system("pause")
+        raise SystemExit
+    if updateresult["assets"] and currentversion > version:
+        print("Downloading new release from: " + downloadlink)
+        if Path("NewChocolateyUpdate.exe").exists():
+            remove("NewChocolateyUpdate.exe")
+        if Path("OldChocolateyUpdate.exe").exists():
+            remove("OldChocolateyUpdate.exe")
+        r = http.request(
+            "GET",
+            downloadlink,
+            headers={"User-Agent": "curl"},
+            timeout=urllib3.Timeout(connect=10.0, read=120.0),
+            retries=2,
+            redirect=True)
+        with open("NewChocolateyUpdate.exe", "wb") as downloadedfile:
+            downloadedfile.write(r.data)
+        rename("ChocolateyUpdate.exe", "OldChocolateyUpdate.exe")
+        system("attrib +h OldChocolateyUpdate.exe")
+        rename("NewChocolateyUpdate.exe", "ChocolateyUpdate.exe")
+        print("Update completed! Restart ChocolateyUpdate to complete!")
+        system("pause")
+        raise SystemExit
+    else:
+        pass
 
 
 admincheck()
